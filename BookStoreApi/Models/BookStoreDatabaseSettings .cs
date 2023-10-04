b9787586209ce09ad780a20b1877d5329f13754b
huynhnvdev <huynhnv@hp-backup.com>
2023-10-04 13:24:47 +0700

    Add project files.

---
+++ BookStoreApi/Models/BookStoreDatabaseSettings .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApi.Models
+{
+    public interface IBookStoreDatabaseSettings
+    {
+        string BooksCollectionName { get; set; }
+        string CollectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+    public class BookStoreDatabaseSettings : IBookStoreDatabaseSettings
+    {
+        public string BooksCollectionName { get; set; }
+        public string CollectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
